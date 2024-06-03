608de2459b48ed5a7319b0bcf1694b6dc6d665d2
Annan Deriba <annanderiba@Annans-MacBook-Air.local>
2024-06-03 19:10:09 +0300

    Added functionality to generate token using agora_toekn_builder

--- base/urls.py
+++ base/urls.py
@@ -7,4 +7,5 @@ app_name = "base"
 urlpatterns = [
     path("", views.lobby, name="lobby"),
     path("room/", views.room, name="room"),
+    path("get_token/", views.get_token, name="get_token"),
 ]
--- base/views.py
+++ base/views.py
@@ -1,4 +1,29 @@
+import random, time
+
 from django.shortcuts import render
+
+from django.http import JsonResponse
+
+from agora_token_builder import RtcTokenBuilder
+
+
+def get_token(request):
+
+    appId = "87adc7c547544480a6b1acaa3cb052bd"
+    appCertificate = "53be29e8e5fa4b97a54b55391b96b332"
+    channelName = request.GET.get("channel")
+    uid = random.randint(1, 230)
+    expirationTimeInSeconds = 3600 * 24
+    currentTimeStamp = time.time()
+    privilegeExpiredTs = currentTimeStamp + expirationTimeInSeconds
+    role = 1  # 1 -- host, 2 -- guest
+
+    # Build token with uid
+    token = RtcTokenBuilder.buildTokenWithUid(
+        appId, appCertificate, channelName, uid, role, privilegeExpiredTs
+    )
+
+    return JsonResponse({"token": token, "uid": uid}, safe=False)
 
 
 def lobby(request):
